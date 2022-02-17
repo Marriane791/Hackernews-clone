3c97c06c62418106b3f67a5311760c7bb1bbcfca
Marriane791 <annemarrieakeyo42@gmail.com>
2022-02-17 23:41:41 +0300

    deleting some packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './Router/AppRouter';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <AppRouter/>
+  ,
+  document.getElementById('root'),
+  document.getElementById('loader')
 );
 
 // If you want to start measuring performance in your app, pass a function
