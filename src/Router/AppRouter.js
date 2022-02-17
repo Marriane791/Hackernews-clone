5223ce02a13de6b32d90f6d3e181722380ed8be2
Marriane791 <annemarrieakeyo42@gmail.com>
2022-02-18 00:10:43 +0300

    changing the main function with a const function ,line spacing ,double quotes,semi colons in line 14 and 13

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -4,27 +4,28 @@ import Header from '../Components/Header'
 import ShowStories from '../Components/ShowStories'
 import PageNotFound from '../Components/PageNotFound'
 
-
-function AppRouter() {
+const AppRouter = () => {
   return (
     <Router>
-        <div className='container'>
-            <Header />
-            <Switch>
-                <Route path="/" render={() => <Redirect to="/top" />} exact={true} />
-                <Route path="/:type" 
-                render={({match}) =>{
-                  const {type} = match.params; // will contain the actual matched value of the type
-                  if(!['top','new','best'].includes(type)) {
-                    return <Redirect to="/" /> ;
-                  }
-                  return <ShowStories type={type} />;
-                }} />
-                <Route component={PageNotFound} />
-            </Switch>
-        </div>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" render={() => <Redirect to="/top" />} exact={true} />
+          <Route
+            path="/:type"
+            render={({ match }) => {
+              const { type } = match.params;
+              if (!['top', 'new', 'best'].includes(type)) {
+                return <Redirect to="/" />;
+              }
+              return <ShowStories type={type} />;
+            }}
+          />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
     </Router>
-  )
-}
+  );
+};
 
 export default AppRouter
