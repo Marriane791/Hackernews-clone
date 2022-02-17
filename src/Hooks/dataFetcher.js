200b9bab650b6d0f001fe451e5f3721bc5a0ff25
Marriane791 <annemarrieakeyo42@gmail.com>
2022-02-18 00:05:32 +0300

    no changes

--- src/Hooks/dataFetcher.js
+++ src/Hooks/dataFetcher.js
@@ -6,20 +6,20 @@ const useDataFetcher = (type) => {
     //declaring two state variables
     const [stories, setStories] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-//maakin the api call
- useEffect(() =>{
-     setIsLoading(true);
-     getStories(type)//when you click the nav menu the type willchange and useeffect hook will run again
-     .then((stories) => {
-         setStories(stories);
-         setIsLoading(false);
-     }
-     ).catch (() => {
-         setIsLoading(false);//incase of an error the loader will be hidden.
-     });
- }, [type]);
- 
- return { isLoading, stories };
-};
+  
+//making the api call
+useEffect(() => {
+    setIsLoading(true);
+    getStories(type) //when you click the nav menu the type willchange and useeffect hook will run again
+      .then((stories) => {
+        setStories(stories);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);//incase of an error the loader will be hidden.
+      });
+  }, [type]);
 
+  return { isLoading, stories };
+};
 export default useDataFetcher;
